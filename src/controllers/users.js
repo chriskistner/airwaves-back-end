2d4e1e70019f6d04af5bb7279d6f90418e14b909
Christopher Kistner <chris.kistner0@gmail.com>
2019-02-10 17:51:28 -0800

    Fixed axios import problem, sucessfully made a post request in Postman and created a new User

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+const axios = require('axios');
 const userModel = require('../models/users');
 const googleUrl = process.env.GOOGLE_GEOCODE_URL
 const key = process.env.GOOGLE_API_KEY
